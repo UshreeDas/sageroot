484ca14e421d3096bc0fdfabe915c946dfad0a46
UshreeDas <ushreedas@gmail.com>
2024-10-24 23:07:25 +0530

    change in carousel

--- src/Components/MainPage/Main.jsx
+++ src/Components/MainPage/Main.jsx
@@ -3,37 +3,76 @@ import "./MainPage.css";
 import University from "../University/University";
 import Why from "../Why-choose/why-choose";
 import Testimonial from "../Testimonials/testimonial";
+
 function Main() {
   return (
     <div>
       <div className="Main">
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel" data-interval="2000" data-pause="false" >
+        {/* Bootstrap Carousel */}
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-ride="carousel"
+          data-interval="2000"
+          data-pause="false"
+        >
           <div className="carousel-inner">
+            {/* First Slide */}
             <div className="carousel-item active">
-              <img className="d-block w-100" src="./John-Hopkins-Business-School.jpg" alt="First slide" className="carousel-img" />
+              <img
+                className="d-block w-100 carousel-img"
+                src="./John-Hopkins-Business-School.jpg"
+                alt="First slide"
+              />
             </div>
+
+            {/* Second Slide */}
             <div className="carousel-item">
-              <img className="d-block w-100" src="./cambridge.jpeg" alt="Second slide" className="carousel-img" />
+              <img
+                className="d-block w-100 carousel-img"
+                src="./cambridge.jpeg"
+                alt="Second slide"
+              />
             </div>
+
+            {/* Third Slide */}
             <div className="carousel-item">
-              <img className="d-block w-100" src="./photo-1617297296495-4af2290c2b84.jpeg" alt="Third slide" className="carousel-img" />
+              <img
+                className="d-block w-100 carousel-img"
+                src="./photo-1617297296495-4af2290c2b84.jpeg"
+                alt="Third slide"
+              />
             </div>
           </div>
-          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+
+          {/* Carousel Controls */}
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
           </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
           </a>
         </div>
       </div>
+
+      {/* Other Components */}
       <University />
       <Why />
       <Testimonial />
     </div>
-  )
+  );
 }
 
 export default Main;
