02a641079091bbf1f9c2352095f220d3b775dc9c
UshreeDas <ushreedas@gmail.com>
2024-10-24 16:41:40 +0530

    edited form

--- src/Components/Form/formContact.jsx
+++ src/Components/Form/formContact.jsx
@@ -49,7 +49,7 @@ const FormContact = () => {
         e.preventDefault();
         // EmailJS sending logic
         emailjs
-            .send('service_x8pd9xp', 'template_14j7pc2', formData, '2zRkZZwe0o7G6CsSE')
+            .send('service_2qon6au', 'template_wy8ypul', formData, 't5UxoD8f8MHhoKvUO')
             .then(
                 (response) => {
                     console.log('SUCCESS!', response.status, response.text);
@@ -74,6 +74,7 @@ const FormContact = () => {
                 }
             );
     };
+    
 
     const subjects = ['Engineering', 'Master of Science', 'Medical', 'Law', 'Business', 'PhD', 'Finance'];
     const countries = ['USA', 'Canada', 'India', 'UK', 'Australia', 'Germany', 'France'];
@@ -86,9 +87,9 @@ const FormContact = () => {
                 <div className="location">
                     <div >
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54674.13627271636!2d77.11782150548234!3d31.078212851078575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390578e3e35d6e67%3A0x1f7e7ff6ff9f54b7!2sShimla%2C%20Himachal%20Pradesh!5e0!3m2!1sen!2sin!4v1729259499606!5m2!1sen!2sin"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3686.041939819465!2d88.39946977507559!3d22.50260957954158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0273fb5271a57f%3A0x536757469ececd6e!2sKidZee%20Madurdaha!5e0!3m2!1sen!2sin!4v1729701561222!5m2!1sen!2sin"
                             width="600"
-                            height="280"
+                            height="380"
                             style={{ border: 0 }}
                             allowFullScreen=""
                             loading="lazy"
@@ -96,18 +97,23 @@ const FormContact = () => {
                             title="Google Map Location"
                         ></iframe>
                     </div>
-                    <div className="contact-info">
-                        <div className="contact-details">
-                            <p className='ico-email'><i class="bi bi-envelope-at"></i> Email: info.sageroots@gmail.com</p>
-                            <p className='ico-number'><i class="bi bi-telephone-fill"></i> Phone: 6291799937</p>
-                        </div>
-                        <div className="social">
+
+                    <div className="social">
+                        <a href="https://wa.me/+916291799937" target="_blank"   >
                             <i class="bi bi-whatsapp"></i>
+                        </a>
+
+                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=info.sageroots@gmail.com" target="_blank"   >
                             <i class="bi bi-envelope"></i>
+                        </a>
+                        <a href="/" target="_blank"   >
                             <i class="bi bi-linkedin"></i>
+                        </a>
+                        <a href="/" target="_blank"   >
                             <i class="bi bi-facebook"></i>
-                        </div>
+                        </a>
                     </div>
+
                 </div>
 
                 <div className="form-container">
@@ -116,102 +122,94 @@ const FormContact = () => {
                         {/* Step 1: Name, Phone, Email */}
                         {step === 1 && (
                             <>
-                                <div className="form-group">
-                                    <label>Name</label>
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        value={formData.name}
-                                        onChange={handleChange}
-                                        placeholder="Enter your name"
-                                        required
-                                    />
+                                <div className="step-devision">
+
+                                    <div className="form-group">
+                                        {/* <label>Name</label> */}
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            value={formData.name}
+                                            onChange={handleChange}
+                                            placeholder="Enter your name"
+                                            required
+                                        />
+                                    </div>
+
+                                    <div className="form-group">
+                                        {/* <label>Phone</label> */}
+                                        <input
+                                            type="tel"
+                                            name="phone"
+                                            value={formData.phone}
+                                            onChange={handleChange}
+                                            placeholder="Enter your phone number"
+                                            required
+                                        />
+                                    </div>
                                 </div>
+                                <div className="step-devision">
+                                    <div className="form-group">
+                                        {/* <label>Email</label> */}
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                            placeholder="Enter your email"
+                                            required
+                                        />
+                                    </div>
 
-                                <div className="form-group">
-                                    <label>Phone</label>
-                                    <input
-                                        type="tel"
-                                        name="phone"
-                                        value={formData.phone}
-                                        onChange={handleChange}
-                                        placeholder="Enter your phone number"
-                                        required
-                                    />
+                                    <div className="form-group">
+                                        {/* <label>Subject Major</label> */}
+                                        <select
+                                            name="subjectMajor"
+                                            value={formData.subjectMajor}
+                                            onChange={handleChange}
+                                            required
+                                        >
+                                            <option value="">Select Program</option>
+                                            {subjects.map((subject) => (
+                                                <option key={subject} value={subject}>
+                                                    {subject}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
                                 </div>
+                                <div className="step-devision">
+                                    <div className="form-group">
+                                        {/* <label>Choice of Country</label> */}
+                                        <select
+                                            name="choiceOfCountry"
+                                            value={formData.choiceOfCountry}
+                                            onChange={handleChange}
+                                            required
+                                        >
+                                            <option value="">Select a country</option>
+                                            {countries.map((country) => (
+                                                <option key={country} value={country}>
+                                                    {country}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </div>
 
-                                <div className="form-group">
-                                    <label>Email</label>
-                                    <input
-                                        type="email"
-                                        name="email"
-                                        value={formData.email}
-                                        onChange={handleChange}
-                                        placeholder="Enter your email"
-                                        required
-                                    />
+                                    <div className="form-group">
+                                        {/* <label>Financial Assistance Required</label> */}
+                                        <select
+                                            name="financialAssistanceRequired"
+                                            value={formData.financialAssistanceRequired}
+                                            onChange={handleChange}
+                                            required
+                                        >
+                                            <option value="">Select</option>
+                                            <option value="yes">Yes</option>
+                                            <option value="no">No</option>
+                                        </select>
+                                    </div>
                                 </div>
-
-                                <button type="button" onClick={handleNextStep} className="next-button">
-                                    Next
-                                </button>
-                            </>
-                        )}
-
-                        {/* Step 2: Subject Major, Choice of Country, Financial Assistance */}
-                        {step === 2 && (
-                            <>
-                                <div className="form-group">
-                                    <label>Subject Major</label>
-                                    <select
-                                        name="subjectMajor"
-                                        value={formData.subjectMajor}
-                                        onChange={handleChange}
-                                        required
-                                    >
-                                        <option value="">Select Program</option>
-                                        {subjects.map((subject) => (
-                                            <option key={subject} value={subject}>
-                                                {subject}
-                                            </option>
-                                        ))}
-                                    </select>
-                                </div>
-
-                                <div className="form-group">
-                                    <label>Choice of Country</label>
-                                    <select
-                                        name="choiceOfCountry"
-                                        value={formData.choiceOfCountry}
-                                        onChange={handleChange}
-                                        required
-                                    >
-                                        <option value="">Select a country</option>
-                                        {countries.map((country) => (
-                                            <option key={country} value={country}>
-                                                {country}
-                                            </option>
-                                        ))}
-                                    </select>
-                                </div>
-
-                                <div className="form-group">
-                                    <label>Financial Assistance Required</label>
-                                    <select
-                                        name="financialAssistanceRequired"
-                                        value={formData.financialAssistanceRequired}
-                                        onChange={handleChange}
-                                        required
-                                    >
-                                        <option value="">Select</option>
-                                        <option value="yes">Yes</option>
-                                        <option value="no">No</option>
-                                    </select>
-                                </div>
-
-                                <button type="button" onClick={handlePreviousStep} className="previous-button">
-                                    Previous
-                                </button>
 
                                 <button type="submit" className="submit-button">
                                     Submit
@@ -219,7 +217,7 @@ const FormContact = () => {
                             </>
                         )}
 
-                        {successMessage && <p className="success-message">{successMessage}</p>}
+                        {/* {successMessage && <p className="success-message">{successMessage}</p>} */}
                     </form>
                 </div>
             </div>
